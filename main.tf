f178c6f453427d6fc6f24b1f5e89d0d0f5505a32
dontslzzzp <brandonard.business@gmail.com>
2024-08-05 08:12:08 -0500

    uncommented alb controller

--- main.tf
+++ main.tf
@@ -24,16 +24,16 @@ module "eks" {
 # AWS ALB Controller
 ################################################################################
 
-# module "aws_alb_controller" {
-#   source = "./modules/aws-alb-controller"
+module "aws_alb_controller" {
+  source = "./modules/aws-alb-controller"
 
-#   main-region  = var.main-region
-#   env_name     = var.env_name
-#   cluster_name = var.cluster_name
+  main-region  = var.main-region
+  env_name     = var.env_name
+  cluster_name = var.cluster_name
 
-#   vpc_id            = module.vpc.vpc_id
-#   oidc_provider_arn = module.eks.oidc_provider_arn
-# }
+  vpc_id            = module.vpc.vpc_id
+  oidc_provider_arn = module.eks.oidc_provider_arn
+}
 
 # ################################################################################
 # # Managed Grafana Module
