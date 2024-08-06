56a7fcf10246193609d9438c3730bb650c3d07dd
dontslzzzp <brandonard.business@gmail.com>
2024-08-05 21:24:26 -0500

    uncommented backend

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
-# terraform {
-#   required_version = ">=0.12.0"
-#   backend "s3" {
-#     key            = "infra/terraformstatefile"
-#     bucket         = "bandzzz-terraform-backend-bucket-us-west-2"
-#     region         = "us-west-2"
-#     dynamodb_table = "terraform-state-locking"
-#     encrypt        = true
-#   }
-# }
+terraform {
+  required_version = ">=0.12.0"
+  backend "s3" {
+    key            = "infra/terraformstatefile"
+    bucket         = "bandzzz-terraform-backend-bucket-us-west-2"
+    region         = "us-west-2"
+    dynamodb_table = "terraform-state-locking"
+    encrypt        = true
+  }
+}
