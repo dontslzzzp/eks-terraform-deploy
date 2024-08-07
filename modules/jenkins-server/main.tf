349428df0e0c83af3ce62d88c1267acbc1661b88
dontslzzzp <brandonard.business@gmail.com>
2024-08-07 02:13:41 -0500

    disabled api termination and stopping for jenkins and terraform instances as well as ignoring changes in instance state

--- modules/jenkins-server/main.tf
+++ modules/jenkins-server/main.tf
@@ -3,6 +3,14 @@ resource "aws_instance" "jenkins_server" {
   instance_type = var.instance_type
   key_name      = var.key_name
   user_data     = file("${path.module}/userdata.sh")
+  disable_api_termination = true
+  disable_api_stop       = true
+  
+  lifecycle {
+    ignore_changes = [
+      instance_state,
+    ]
+  }
 
   tags = {
     Name = "jenkins_server"
--- modules/terraform_node/main.tf
+++ modules/terraform_node/main.tf
@@ -3,6 +3,14 @@ resource "aws_instance" "terraform_node" {
   instance_type = var.instance_type
   key_name      = var.key_name
   user_data     = file("${path.module}/userdata.sh")
+  disable_api_termination = true
+  disable_api_stop       = true
+  
+  lifecycle {
+    ignore_changes = [
+      instance_state,
+    ]
+  }
 
   tags = {
     Name = "terraform_node"
